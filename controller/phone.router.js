64beb31e42b76adc61d30e4115083679d01037dd
steven29122004 <165149053+steven29122004@users.noreply.github.com>
2025-05-15 14:06:09 +0700

    done for Form

--- controller/phone.router.js
+++ controller/phone.router.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const pController = require('./phone.controller');
 
 router.get('/', pController.getPage);
-router.get('/form', pController.getForm);
-router.get('/form1', pController.getForm1);
+router.get('/form', pController.getForm1);
 router.post('/form', pController.postForm);
 router.get('/:id', pController.getDetail);
 router.delete('/', pController.deleteProduct)
