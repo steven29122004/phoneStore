8ae2f2f036099a3f2b495526c8e855a95b145c0e
steven29122004 <165149053+steven29122004@users.noreply.github.com>
2025-05-16 18:28:30 +0700

    done with modal Btn and post

--- controller/phone.router.js
+++ controller/phone.router.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const pController = require('./phone.controller');
 
 router.get('/', pController.getPage);
-router.get('/form', pController.getForm);
-router.post('/form', pController.postForm);
+router.post('/', pController.postForm);
 router.get('/detail/:id', pController.getDetail);
 router.get('/api/detail/:id', pController.getDetailAPI);
 router.delete('/:id', pController.deleteProduct)
